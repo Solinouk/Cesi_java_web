1d900e3004e541ea520c7f8b7c5263526b5f9dd8
SOLENNE DIDRON <solenne.didron@viacesi.fr>
2020-12-01 13:10:53 +0100

    First commit

---
+++ projetWeb/src/test/java/cesi/java/projetWeb/ProjetWebApplicationTests.java
@@ -0,0 +1,13 @@
+package cesi.java.projetWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
